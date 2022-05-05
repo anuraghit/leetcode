10d2e83d3523396079eb66ab6bbc985685acd596
Anurag Sharma <57986981+anuraghit@users.noreply.github.com>
2022-05-05 18:44:02 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (99.31%) - LeetHub

---
+++ 367-valid-perfect-square/367-valid-perfect-square.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        
+        long left=1;
+        long right=(num/2)+1;
+        long mid=0;
+        
+        while(left<=right)
+        {
+            mid=left+(right-left)/2;
+            
+            if((mid*mid)==num)
+            {
+                return true;
+            }
+            if((mid*mid)<num)
+            {
+                left=mid+1;
+            }
+            else
+            {
+                right=mid-1;
+            }
+        }
+        return false;
+        
+    }
+};
